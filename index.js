7b7be78223dccce5dd7c81f22a62381cf4ab7faa
Renato <reytuty@gmail.com>
2019-03-18 17:28:48 -0300

    now can force checkSum value

--- index.js
+++ index.js
@@ -3,13 +3,17 @@
  * @author Renato Miawaki
  * @email renato.miawaki@gmail.com 
  */
-const Buffer        = require('buffer').Buffer ;
-const nmeaCheck     = require('nmea-checksum') ;
+const Buffer                = require('buffer').Buffer ;
+const nmeaCheck             = require('nmea-checksum') ;
 
-const getMessageBuffer = (header, m)=>{
+const getMessageBuffer = (header, m, forceCheckSum = null )=>{
     //message size + 12 before $
     var len = m.length + 12 ;
-    var s = "  "+len+"$"+header+","+m+"*" +nmeaCheck.checksum(m);
+    var s = "  "+len+"$"+header+","+m+"*";
+    if(!forceCheckSum) {
+        forceCheckSum = nmeaCheck.checksum(m);
+    }
+    s += forceCheckSum ;
     return bufferToUint8( string2Buffer(s) );
 }
 const string2Buffer = (s)=>{
@@ -19,19 +23,24 @@ const string2Buffer = (s)=>{
     }
     message.push(13) ;
     message.push(10) ;
-    return Buffer.from(message);
+    return Buffer.from(message); 
 }
 const bufferToUint8 = (buf)=>{
     return new Uint8Array(buf) ;
 }
+/**
+ * Send just once message 
+ * @param {*} data 
+ * @param {*} message2Array 
+ */
 const parse = (data, message2Array = true)=>{
     var m = data.toString();
     m = m.split("$");
     m.shift() ;
-    m = m.join("").split("*") 
+    m = m.join("").split("*") ;
     m.pop() ;
     m = m.join("").split(",") ;
-    const header = m.shift();
+    const header = m.shift() ;
     const message = (message2Array)?m:m.join(",") ;
     return {header, message}
 }
